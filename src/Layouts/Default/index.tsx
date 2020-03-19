f675f7ccc03363330d0547db8bbb1d8da475b479
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-20 01:33:43 +0330

    feat(view): add layouts

---
+++ src/Layouts/Default/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import { Sidebar, DashboardContent } from '../../Components'
+
+const DefaultLayout: FC = (props) => {
+    return (
+        <main className="flex">
+            <Sidebar />
+            <DashboardContent>
+                {props.children}
+            </DashboardContent>
+        </main>
+    )
+}
+
+export default DefaultLayout;
