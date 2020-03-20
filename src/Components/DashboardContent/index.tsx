b024c4d65b623a196edd58c2401487bd511165d7
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-20 17:25:55 +0330

    fix(DashboardContent): change bg color to gray

--- src/Components/DashboardContent/index.tsx
+++ src/Components/DashboardContent/index.tsx
@@ -6,7 +6,7 @@ type props = {
 
 export default (props:props) => {
     return (
-        <section className="relative md:ml-64 py-8 px-4 bg-indigo-100 min-h-screen w-full">
+        <section className="relative md:ml-64 py-8 px-4 bg-gray-100 min-h-screen w-full">
             {props.children}
         </section>
     )
