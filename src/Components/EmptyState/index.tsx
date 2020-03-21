59e850fa49a91076002a4fd900f900a04c187ea5
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 22:36:42 +0430

    fix: a component for showing empty states

---
+++ src/Components/EmptyState/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type props = {
+    image: string;
+    text: string;
+}
+
+export default (props: props) => {
+    return (
+        <div className="flex flex-col justify-center items-center h-full w-full">
+            <img src={props.image} alt="" className="max-w-lg" />
+            <p className="text-gray-600 text-xl">
+                {props.text}
+            </p>
+        </div>
+    )
+}
