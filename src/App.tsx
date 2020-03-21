5fe3d6e3d4237cb054d62c3faf0b8aa8297645a9
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 22:39:28 +0430

    fix: implement react-router and handle managing context data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { TodoContext, TaskType, ListType, GroupType } from './context';
 import { DefaultLayout } from './Layouts'
+import { ImportantView, MyDayView, TasksView, ListView } from './Views'
 
 function App() {
+  const [tasks, setTasks] = useState<Array<TaskType>>([]);
+  const [lists, setLists] = useState<Array<ListType>>([]);
+  const [groups, setGroups] = useState<Array<GroupType>>([]);
+
+  // task functions
+  function addTask(task: TaskType) {
+    const newTasks = tasks.concat(task);
+    setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  }
+
+  function editTask(taskID: string) { }
+
+  function deleteTask(taskID: string) { }
+
+  // list functions
+  function addList(list: ListType) { }
+
+  function editList(listID: string) { }
+
+  function deleteList(listID: string) { }
+
+  // group functions
+  function addGroup(group: GroupType) { }
+
+  function editGroup(groupID: string) { }
+
+  function deleteGroup(groupID: string) { }
+
+  useEffect(() => {
+    const localTasks = localStorage.getItem('tasks');
+    if (localTasks) {
+      setTasks(JSON.parse(localTasks))
+    }
+  }, [])
+
   return (
-    <DefaultLayout>
-      <p>Hi there!</p>
-    </DefaultLayout>
+    <TodoContext.Provider value={{
+      tasks,
+      lists,
+      groups,
+      addTask,
+      editTask,
+      deleteTask,
+      addList,
+      editList,
+      deleteList,
+      addGroup,
+      editGroup,
+      deleteGroup
+    }}>
+      <BrowserRouter>
+        <DefaultLayout>
+          <Switch>
+            <Route
+              path="/"
+              render={() => <Redirect to="/tasks" />}
+              exact
+            />
+            <Route
+              path="/tasks"
+              component={TasksView}
+              exact
+            />
+            <Route
+              path="/tasks/list/:id"
+              component={ListView}
+            />
+            <Route
+              path="/tasks/important"
+              component={ImportantView}
+            />
+            <Route
+              path="/tasks/myday"
+              component={MyDayView}
+            />
+          </Switch>
+        </DefaultLayout>
+      </BrowserRouter>
+    </TodoContext.Provider>
   );
 }
 
---
+++ src/context.ts
@@ -0,0 +1,55 @@
+import { createContext } from 'react'
+
+export type TaskType = {
+    id: string;
+    task: string;
+    done: boolean;
+    important: boolean;
+    myday: boolean;
+    inList: boolean;
+}
+
+export type ListType = {
+    id: string;
+    tasks: Array<TaskType>,
+    inGroup: boolean;
+}
+
+export type GroupType = {
+    id: string;
+    lists: Array<ListType>;
+}
+
+const tasks: Array<TaskType> = [];
+const lists: Array<ListType> = [];
+const groups: Array<GroupType> = [];
+
+const addTask: Function = () => { };
+const editTask: Function = () => { };
+const deleteTask: Function = () => { };
+
+const addList: Function = () => { };
+const editList: Function = () => { };
+const deleteList: Function = () => { };
+
+const addGroup: Function = () => { };
+const editGroup: Function = () => { };
+const deleteGroup: Function = () => { };
+
+export const TodoContext = createContext({
+    tasks,
+    lists,
+    groups,
+
+    addTask,
+    editTask,
+    deleteTask,
+
+    addList,
+    editList,
+    deleteList,
+
+    addGroup,
+    editGroup,
+    deleteGroup
+})
