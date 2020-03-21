232f9c22efe3185e597c82eb43db7d6bd83b1efb
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 22:38:50 +0430

    fix: empty-test for custom list tasks

---
+++ src/Views/List/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ContentTitle, TaskRowsContainer, TaskRow } from '../../Components'
+
+export default () => {
+    return (
+        <>
+            <ContentTitle
+                title="My custom list"
+            />
+            <TaskRowsContainer>
+                <TaskRow
+                    task="My Day task"
+                    id="1243"
+                    done={false}
+                    important={true}
+                    myDay={true}
+                    onDone={() => { }}
+                    onImportantClick={() => { }}
+                    onMyDayClick={() => { }}
+                />
+            </TaskRowsContainer>
+        </>
+    )
+}
