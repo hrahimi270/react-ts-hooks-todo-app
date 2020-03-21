ed8b5540a75961e6747814cf8b6ea7c4da2ee1ea
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 13:22:26 +0430

    feat(view): custom checkbox component

---
+++ src/Components/RowCheckbox/index.tsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components';
+
+const checkFrames = keyframes`
+    50% {
+        transform: scale(1.2);
+    }
+`;
+const CheckBoxWrapper = styled.div``;
+const CheckboxLabelSvgParent = styled.span`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: translate3d(0, 0, 0);
+
+    position: relative;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    transform: scale(1);
+    border: 1px solid #a0aec0;
+    transition: all 0.2s ease;
+
+    &:before {
+        content: "";
+        width: 100%;
+        height: 100%;
+        background: #506EEC;
+        display: block;
+        transform: scale(0);
+        opacity: 1;
+        border-radius: 50%;
+        transition-delay: 0.2s;
+    }
+
+    svg {
+        position: absolute;
+        z-index: 1;
+        fill: none;
+        stroke: white;
+        stroke-width: 2;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke-dasharray: 16px;
+        stroke-dashoffset: 16px;
+        transition: all 0.3s ease;
+        transition-delay: 0.1s;
+        transform: translate3d(0, 0, 0);
+    }
+`;
+const CheckboxLabelText = styled.span`
+    display: flex;
+    align-items: center;
+    margin-left: 8px;
+    color: #1a202c;
+    transform: translate3d(0, 0, 0);
+
+    &:after {
+        content: "";
+        position: absolute;
+        height: 1px;
+        width: 100%;
+        background: #a0aec0;
+        transform-origin: 0 0;
+        transform: scaleX(0);
+    }
+`;
+const CheckboxLabel = styled.label`
+    display: flex;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+    cursor: pointer;
+
+    &:hover {
+        ${CheckboxLabelSvgParent} {
+            border-color: #3182ce;
+        }
+    }
+`;
+const CheckboxInput = styled.input`
+    display: none;
+
+    &:checked + ${CheckboxLabel} ${CheckboxLabelSvgParent} {
+        border-color: #3182ce;
+        background: #3182ce;
+        animation: ${checkFrames} 0.6s ease;
+
+        svg {
+            stroke-dashoffset: 0;
+        }
+
+        &:before {
+            transform: scale(2.2);
+            opacity: 0;
+            transition: all 0.6s ease;
+        }
+    }
+
+    &:checked + ${CheckboxLabel} ${CheckboxLabelText} {
+        color: #a0aec0;
+        transition: all 0.3s ease;
+
+        &:after {
+            transform: scaleX(1);
+            transition: all 0.3s ease;
+        }
+    }
+`;
+
+type props = {
+    label: string;
+    labelFor: string;
+    checked: boolean;
+    onChange: Function;
+}
+
+export default (props: props) => {
+    return (
+        <CheckBoxWrapper>
+            <CheckboxInput type="checkbox" id={props.labelFor} checked={props.checked} onChange={() => props.onChange()} />
+            <CheckboxLabel htmlFor={props.labelFor}>
+                <CheckboxLabelSvgParent>
+                    <svg viewBox="0 0 12 9" width="12px" height="9px">
+                        <polyline points="1 5 4 8 11 1"></polyline>
+                    </svg>
+                </CheckboxLabelSvgParent>
+                <CheckboxLabelText>
+                    {props.label}
+                </CheckboxLabelText>
+            </CheckboxLabel>
+        </CheckBoxWrapper>
+    )
+}
