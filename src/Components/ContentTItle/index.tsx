c6919a0773f8405f0bfd46c08c9f2aaba0a70234
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 13:21:54 +0430

    feat(view): add title component for content section

---
+++ src/Components/ContentTItle/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type props = {
+    title: string;
+    color?: "red" | "orange" | "yellow" | "green" | "blue" | "purple" | "pink" | "gray";
+}
+
+export default (props: props) => {
+    const { color = 'gray' } = props;
+    const titleCLassName = `text-2xl font-bold text-${color}-500 mb-4 mt-8`;
+    
+    return (
+        <h1 className={titleCLassName}>{props.title}</h1>
+    )
+}
