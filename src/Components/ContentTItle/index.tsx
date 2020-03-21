f6e6a96ebec675ee768efda82c9c2a4be5e9de72
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-21 22:36:29 +0430

    fix: spacing

--- src/Components/ContentTItle/index.tsx
+++ src/Components/ContentTItle/index.tsx
@@ -7,7 +7,7 @@ type props = {
 
 export default (props: props) => {
     const { color = 'gray' } = props;
-    const titleCLassName = `text-2xl font-bold text-${color}-500 mb-4 mt-8`;
+    const titleCLassName = `text-2xl font-bold text-${color}-700 mb-4 mt-8`;
     
     return (
         <h1 className={titleCLassName}>{props.title}</h1>
