d1a06b1c10b1df84380d6f18a874f359ab1a9b2b
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-22 19:24:52 +0430

    feat(content title): add delete button for custom lists

--- src/Components/ContentTItle/index.tsx
+++ src/Components/ContentTItle/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { useParams, useHistory } from 'react-router-dom'
+import { FiTrash } from 'react-icons/fi'
+import { TodoContext } from "../../context";
 
 type props = {
 	title: string;
-	color?:
-		| "red"
-		| "orange"
-		| "yellow"
-		| "green"
-		| "blue"
-		| "purple"
-		| "pink"
-		| "gray";
 };
 
 export default (props: props) => {
-	const { color = "gray" } = props;
-	const titleCLassName = `text-2xl font-bold text-${color}-700 mb-4 mt-8`;
+	const { push } = useHistory();
+	const { id } = useParams();
 
-	return <h1 className={titleCLassName}>{props.title}</h1>;
+	return (
+		<div className="flex items-center justify-between">
+			<h1 className="text-2xl font-bold text-gray-700 mb-4 mt-8">
+				{props.title}
+			</h1>
+			
+			{id && id !== '' ? (
+				<TodoContext.Consumer>
+					{(({ deleteList }) => (
+						<button className="flex p-3 text-2xl hover:text-red-600 focus:outline-none" onClick={() => deleteList(id, push('/tasks'))}>
+							<FiTrash />
+						</button>
+					))}
+				</TodoContext.Consumer>
+			) : null}
+		</div>
+	);
 };
