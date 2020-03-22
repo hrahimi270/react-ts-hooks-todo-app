f24e3adae513593e40adcc9606606b98bf896a1e
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-22 19:25:46 +0430

    feat(List): show tasks of a custom list

--- src/Views/List/index.tsx
+++ src/Views/List/index.tsx
@@ -1,21 +1,64 @@
 import React from "react";
-import { ContentTitle, TaskRowsContainer, TaskRow } from "../../Components";
+import { useParams } from 'react-router-dom'
+import { ContentTitle, TaskRowsContainer, TaskRow, AddTask, EmptyState } from "../../Components";
+import { TodoContext } from "../../context";
+import tasksImage from "../../Statics/empty-tasks.svg";
 
 export default () => {
+	const { id } = useParams();
+
 	return (
 		<>
-			<ContentTitle title="My custom list" />
+			<TodoContext.Consumer>
+				{(({ lists }) => {
+					const activeList = lists.filter((list) => list.id === id)[0];
+					const name = activeList.name ? activeList.name : '';
+					
+					return <ContentTitle title={name} />;
+				})}
+			</TodoContext.Consumer>
 			<TaskRowsContainer>
-				<TaskRow
-					task="My Day task"
-					id="1243"
-					done={false}
-					important={true}
-					myDay={true}
-					onEdit={() => { }}
-					onDeleteClick={() => { }}
-				/>
+				<TodoContext.Consumer>
+					{({ tasks, editTask, deleteTask }) => {
+						const filteredTasks = tasks.filter(
+							(task) => task.listID === id,
+						);
+
+						return filteredTasks.length ? (
+							filteredTasks.map((task) => {
+								return (
+									<TaskRow
+										key={task.id}
+										task={task.task}
+										id={task.id}
+										done={task.done}
+										important={task.important}
+										myDay={task.myday}
+										onEdit={editTask}
+										onDeleteClick={deleteTask}
+									/>
+								);
+							})
+						) : (
+								<EmptyState
+									image={tasksImage}
+									text="There is no task in this list!"
+								/>
+							);
+					}}
+				</TodoContext.Consumer>
 			</TaskRowsContainer>
+
+			<TodoContext.Consumer>
+				{({ addTask }) => (
+					<AddTask
+						onAdd={addTask}
+						listID={id}
+						isImportant={false}
+						isMyday={false}
+					/>
+				)}
+			</TodoContext.Consumer>
 		</>
 	);
 };
