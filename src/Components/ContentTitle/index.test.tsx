b39953e91a1585237d316a514c87758a380af755
Hossein Rahimi <hrahimi270@gmail.com>
2020-03-23 20:48:18 +0430

    feat: implement test for ContentTitle component

---
+++ src/Components/ContentTitle/index.test.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ContentTItle from "../ContentTitle";
+import { TodoContext, tasks, lists } from "../../context";
+
+jest.mock("react-router-dom", () => ({
+	useHistory: () => ({
+		push: jest.fn(),
+	}),
+	useParams: () => ({
+		id: Date.now(),
+	}),
+}));
+
+function renderContentTItle(
+	title: string,
+	inCustomList: boolean,
+	deleteList: Function,
+) {
+	const addTask: Function = jest.fn();
+	const editTask: Function = jest.fn();
+	const deleteTask: Function = jest.fn();
+	const addList: Function = jest.fn();
+	const editList: Function = jest.fn();
+
+	return render(
+		<TodoContext.Provider
+			value={{
+				tasks,
+				lists,
+				addTask,
+				editTask,
+				deleteTask,
+				addList,
+				editList,
+				deleteList,
+			}}
+		>
+			<ContentTItle title={title} inCustomList={inCustomList} />
+		</TodoContext.Provider>,
+	);
+}
+
+test("ContentTitle component without list", () => {
+	const title = "My Custom list";
+	const { container, getByText } = renderContentTItle(title, false, () => {});
+	const h1 = getByText(title);
+	const button = container.querySelector("button");
+
+	expect(h1).toHaveTextContent(title);
+	expect(button).toBeNull();
+});
+
+test("ContentTitle component with list", () => {
+	const deleteListMock = jest.fn((data) => data);
+	const title = "My Custom list";
+	const { container, getByText } = renderContentTItle(
+		title,
+		true,
+		deleteListMock,
+	);
+	const h1 = getByText(title);
+	const button = container.querySelector("button");
+
+	expect(h1).toHaveTextContent(title);
+	expect(button).not.toBeNull();
+	userEvent.click(container.getElementsByTagName("button")[0]);
+
+	const result = deleteListMock.mock.results[0].value;
+	expect(result).toBeTruthy();
+});
